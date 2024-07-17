5382b37cff4bcd9ec25c0252a07789441bc6310f
paru <money_path@outlook.com>
2024-07-17 18:09:14 +0800

    仓库建立

---
+++ src/main/java/com/multi/paru/ParuApplication.java
@@ -0,0 +1,13 @@
+package com.multi.paru;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParuApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ParuApplication.class, args);
+    }
+
+}
